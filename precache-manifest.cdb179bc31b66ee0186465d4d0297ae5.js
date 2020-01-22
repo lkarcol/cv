6792a06c0a367d1c1072a44db4b28f2434be42f5
Lukas <lkkarcol@gmail.com>
2020-01-22 18:22:05 +0100

    Updates

---
+++ precache-manifest.cdb179bc31b66ee0186465d4d0297ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68343cbddf7e4d593d338248f78ad572",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "c96b2fb20c5b1e8d5d7e",
+    "url": "/cv/static/js/2.c149258d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/cv/static/js/2.c149258d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39220d324b6b90d9dac8",
+    "url": "/cv/static/js/main.aef57d1e.chunk.js"
+  },
+  {
+    "revision": "9cc8135adf80447f509a",
+    "url": "/cv/static/js/runtime-main.7826774c.js"
+  }
+]);
